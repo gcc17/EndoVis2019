29d91d07f370e2672f652282fecf644d36a08ccf
dreamguo <836918658@qq.com>
2019-08-16 12:57:59 +0800

    join utils.py

--- utils.py
+++ utils.py
@@ -2,6 +2,12 @@ import os
 import torch
 import numpy as np
 import random
+import torch.nn as nn
+from PIL import Image
+import matplotlib.pyplot as plt
+from matplotlib import gridspec
+import subprocess
+from config import *
 
 feature_dir = './i3d/rbg'
 #feature_tmp = np.load('./i3d/rgb/Hei-Chole1-rgb.npz')
@@ -37,9 +43,30 @@ def get_train_cases(feature_name_list, feature_type='rgb', length=512):
         feature = feature[start_frame : start_frame+ feature]
         train_cases.append(feature)
     return train_cases
-    
+
 def get_gt():
     gt_cases = []
 
     return gt_cases
+
+def get_phase_error(pred_phase, gt_phase):
+    criterion = nn.CrossEntropyLoss()
+    pred_phase = torch.autograd.Variable(torch.FloatTensor([pred_phase]))
+    gt_phase = torch.autograd.Variable(torch.FloatTensor([gt_phase]))
+    loss = criterion(pred_phase, gt_phase)
+    return loss.mean().item()
+
+def get_instrument_error(pred_instrument, gt_instrument):
+    criterion = nn.MultiLabelSoftMarginLoss()
+    pred_instrument = torch.autograd.Variable(torch.FloatTensor([pred_instrument]))
+    gt_instrument = torch.autograd.Variable(torch.FloatTensor([gt_instrument])
+    loss = criterion(pred_instrument, gt_instrument)
+    return loss.mean()
+
+def get_action_error(pred_action, gt_action):
+    criterion = nn.MultiLabelSoftMarginLoss()  
+    pred_action = torch.autograd.Variable(torch.FloatTensor([pred_action]))
+    gt_action = torch.autograd.Variable(torch.FloatTensor([gt_action]))
+    loss = criterion(pred_action, gt_action)
+    return loss.mean() 
 
