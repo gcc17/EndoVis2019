4419dfe04c8c9cc58b74d638512c43bdf1d48af9
gcc17 <46296716+gcc17@users.noreply.github.com>
2019-08-19 11:10:46 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -7,42 +7,44 @@ from my_dataset import TrainDataset, TestDataset
 from model import TCNNet, GRUNet
 import os
 from config import *
-from utils import get_phase_error, get_instrument_error, get_action_error
+from utils import get_phase_error, get_instrument_error, get_action_error, num2name
 import pdb
+
 ######################
 
 locals().update(training_params)
 
+
 ######################
 
-# test的损失函数怎么表示？每一帧的loss的平均？
-def test(model, test_loader):
 
+def test(model, test_loader):
     model.eval()
-    test_phase = []
-    gt_phase = []
-    test_instrument = []
-    gt_instrument = []
-    test_action = []
-    gt_action = []
+    phase_error = 0.0
+    instrument_error = 0.0
+    action_error = 0.0
+    test_num = 0
 
     with torch.no_grad:
-        for _, data in enumerate(test_loader):
+        for num, data in enumerate(test_loader):
 
             feature = data['data'].cuda().float()
             pred_phase, pred_instrument, pred_action = model(feature)
-            test_phase.append(pred_phase)
-            gt_phase.append(data['phase'].cuda().float())
-            test_instrument.append(pred_instrument)
-            gt_instrument.append(data['instrument'].cuda().float())
-            test_action.append(pred_action)
-            gt_action.append(data['action'].cuda().float())
+            is_test_case = data['is_test_case'].item()
 
-    return get_phase_error(test_phase, gt_phase), get_instrument_error(test_instrument, gt_instrument), get_action_error(test_action, gt_action)
+            if is_test_case:
+                phase_error += get_phase_error(pred_phase,
+                                               data['phase'].cuda().float())
+                instrument_error += get_instrument_error(pred_instrument, data[
+                    'instrument'].cuda().float())
+                action_error += get_action_error(pred_action,
+                                                 data['action'].cuda().float())
+                test_num = num + 1
+
+    return phase_error / test_num, instrument_error / test_num, action_error / test_num
 
 
 def train(model, train_loader, test_loader, naming, use_tf_log):
-
     model_dir = os.path.join('./models', naming)
     if not os.path.exists(model_dir):
         os.makedirs(model_dir)
@@ -60,14 +62,14 @@ def train(model, train_loader, test_loader, naming, use_tf_log):
                    'test_phase': [], 'test_instrument': [], 'test_action': []}
 
     # loss function and optimizer
-    # 因为用的是交叉熵损失函数，阶段的预测也要用one-hot
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate,
+                           weight_decay=weight_decay)
 
     optimizer.zero_grad()
 
     phase_criterion = nn.CrossEntropyLoss()
-    instrument_criterion = nn.MultiLabelSoftMarginLoss()
-    action_criterion = nn.MultiLabelSoftMarginLoss()
+    instrument_criterion = nn.BCEWithLogitsLoss
+    action_criterion = nn.BCEWithLogitsLoss()
 
     step = 0
 
@@ -80,7 +82,8 @@ def train(model, train_loader, test_loader, naming, use_tf_log):
             loss_test_phase, loss_test_instrument, loss_test_action = test(
                 model, test_loader)
             print('{} test_phase:{} test_instrument:{} test_action:{}'.format(
-                naming, loss_test_phase, loss_test_instrument, loss_test_action))
+                naming, loss_test_phase, loss_test_instrument,
+                loss_test_action))
 
             if use_tf_log:
                 logger.scalar_summary('test_phase', loss_test_phase, step)
@@ -91,17 +94,18 @@ def train(model, train_loader, test_loader, naming, use_tf_log):
             result_dict['test_instrument'].append(loss_test_instrument)
             result_dict['test_action'].append(loss_test_action)
 
-            torch.save(model.state_dict(), os.path.join(model_dir, 'model'),)
+            torch.save(model.state_dict(), os.path.join(model_dir, 'model'), )
 
         model.train()
         gt_phase = data['phase'].cuda().float()
-        gt_instrument = [data['instrument'].cuda().float()]
-        gt_action = [data['action'].cuda().float()]
+        gt_instrument = data['instrument'].cuda().float()
+        gt_action = data['action'].cuda().float()
         feature = data['full_data'].cuda().float()
 
         pdb.set_trace()
 
         pred_phase, pred_instrument, pred_action = model(feature)
+        # 训练的时候loss该如何计算？输出维度为B×T×C，损失函数要求的输入为B×C
         loss_phase = phase_criterion(pred_phase, gt_phase)
         loss_phase = loss_phase.mean()
         loss_instrument = instrument_criterion(pred_instrument, gt_instrument)
@@ -134,45 +138,49 @@ def train(model, train_loader, test_loader, naming, use_tf_log):
         step += 1
         if step >= max_step_num:
             break
-    np.save(os.path.join(log_dir, naming+'.npy'), result_dict)
+    np.save(os.path.join(log_dir, naming + '.npy'), result_dict)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('--feature_file', type=str)
+    parser.add_argument('--feature_type', type=str)
     parser.add_argument('--use_tf_log', type=int)
+    parser.add_argument('--train', action='append')
+    parser.add_argument('--test', action='append')
+
 
     args = parser.parse_args()
 
-    feature_file = args.feature_file
+    feature_type = args.feature_type
     use_tf_log = args.use_tf_log
+    train_list = args.train
+    test_list = args.test
+    train_name_list = num2name(train_list, feature_type)
+    test_name_list = num2name(test_list, feature_type)
 
-    # load all data!
-    all_data = np.load(feature_file, allow_pickle=True).item()
+    naming = '{}-train_{}'.format(naming_prefix, train_list,)
+    print('Feature_type:    {}'.format(feature_type))
+    print('TFLog:           {}'.format(use_tf_log))
+    print('Naming:          {}'.format(naming))
 
-    for repeat_id in range(split_repeat):
-        for split_id in range(split_num):
-            naming = '{}-{}-repeat_{}-split_{}'.format(naming_prefix, feature_file.split('/')[-1], repeat_id, split_id)
-            print('Repeat:      {}'.format(repeat_id))
-            print('Split:       {}'.format(split_id))
-            print('Feature:     {}'.format(feature_file))
-            print('TFLog:       {}'.format(use_tf_log))
-            print('Naming:      {}'.format(naming))
+    if model_type == 'TCN':
+        model = TCNNet(input_dim=input_dim, dropout_rate=dropout_rate).cuda()
 
-            if model_type == 'TCN':
-                model = TCNNet(input_dim=input_dim, dropout_rate=dropout_rate).cuda()
+    elif model_type == 'GRU':
+        model = GRUNet(input_dim=input_dim, dropout_rate=dropout_rate).cuda()
 
-            elif model_type == 'GRU':
-                model = GRUNet(input_dim=input_dim, dropout_rate=dropout_rate).cuda()
+    else:
+        raise Exception('Unknown Model Type.')
 
-            else:
-                raise Exception('Unknown Model Type.')
+    train_dataset = TrainDataset(train_name_list, feature_type)
+    test_dataset = TestDataset(test_name_list, feature_type)
+    train_loader = torch.utils.data.DataLoader(train_dataset,
+                                               batch_size=batch_size,
+                                               shuffle=True, drop_last=True)
+    test_loader = torch.utils.data.DataLoader(test_dataset,
+                                              batch_size=batch_size,
+                                              shuffle=False)
 
-            train_dataset = TrainDataset(repeat_id, split_id, all_data)
-            test_dataset = TestDataset(repeat_id, split_id, all_data)
-            train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
-            test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
-
-            train(model, train_loader, test_loader, naming, use_tf_log)
+    train(model, train_loader, test_loader, naming, use_tf_log)
