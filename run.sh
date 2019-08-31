edfbf80729034ac83bbda4ace868c6c2fba72136
gcc17 <46296716+gcc17@users.noreply.github.com>
2019-08-31 17:29:41 +0800

    Delete run.sh

--- run.sh
+++
@@ -1,4 +0,0 @@
-#my.sh
-#!/bin/bash
-
-CUDA_VISIBLE_DEVICES=1 python3 main.py --feature_type flow_oversample_4 --use_tf_log 1 --train 1 --train 2 --train 3 --train 4 --train 5 --train 6 --train 7 --train 8 --train 9 --test 10 --test 11 --test 12
